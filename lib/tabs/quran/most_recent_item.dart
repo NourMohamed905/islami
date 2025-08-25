c92213799fab9b71d0d21614144a9a965fa50fe8
NourMohamed905 <farouk.graduate2022@gmail.com>
2025-08-25 19:31:20 +0300

    add clicabel in most recently

--- lib/tabs/quran/most_recent_item.dart
+++ lib/tabs/quran/most_recent_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami/app_theme.dart';
 import 'package:islami/tabs/quran/sura.dart';
+import 'package:islami/tabs/quran/sura_details_screen.dart';
 
 class MostRecentItem extends StatelessWidget {
   Sura sura;
@@ -11,47 +12,53 @@ class MostRecentItem extends StatelessWidget {
     TextTheme textTheme = Theme.of(context).textTheme;
     Size screenSize = MediaQuery.of(context).size;
 
-    return Container(
-      padding: EdgeInsets.only(left: 12, top: 8, right: 8, bottom: 8),
-      decoration: BoxDecoration(
-        color: AppTheme.primary,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      height: double.infinity,
-      width: screenSize.width * 0.7,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                sura.englishName,
-                style: textTheme.headlineMedium!.copyWith(
-                  color: AppTheme.black,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(SuraDetailsScreen.routeName, arguments: sura);
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 12, top: 8, right: 8, bottom: 8),
+        decoration: BoxDecoration(
+          color: AppTheme.primary,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        height: double.infinity,
+        width: screenSize.width * 0.7,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  sura.englishName,
+                  style: textTheme.headlineMedium!.copyWith(
+                    color: AppTheme.black,
+                  ),
                 ),
-              ),
-              Text(
-                sura.arabicName,
-                style: textTheme.headlineMedium!.copyWith(
-                  color: AppTheme.black,
+                Text(
+                  sura.arabicName,
+                  style: textTheme.headlineMedium!.copyWith(
+                    color: AppTheme.black,
+                  ),
                 ),
-              ),
-              Text(
-                '${sura.ayatCount} Verses',
-                style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                      color: AppTheme.black,
-                    ),
-              ),
-            ],
-          ),
-          Image.asset(
-            'assets/images/most_recent.png',
-            width: screenSize.width * 0.3,
-            height: screenSize.width * 0.3,
-            fit: BoxFit.fill,
-          ),
-        ],
+                Text(
+                  '${sura.ayatCount} Verses',
+                  style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                        color: AppTheme.black,
+                      ),
+                ),
+              ],
+            ),
+            Image.asset(
+              'assets/images/most_recent.png',
+              width: screenSize.width * 0.3,
+              height: screenSize.width * 0.3,
+              fit: BoxFit.fill,
+            ),
+          ],
+        ),
       ),
     );
   }
